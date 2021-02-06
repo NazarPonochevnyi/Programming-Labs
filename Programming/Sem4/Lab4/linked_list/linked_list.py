bd0f5c7f1c58a42702054453e55f9a4d31bb7485
NazarPonochevnyi <nazar.ponochevnyi@gmail.com>
2021-02-06 16:23:03 +0200

    fix linkedlist to doublylinkedlist

--- Programming/Sem4/Lab4/linked_list/linked_list.py
+++ Programming/Sem4/Lab4/linked_list/linked_list.py
@@ -2,6 +2,7 @@
 
 class Node:
     def __init__(self, data):
+        self.prev = None
         self.data = data
         self.next = None
 
@@ -14,17 +15,24 @@ class Node:
 
 class LinkedList:
     def __init__(self, nodes=None):
-        self.head = None
+        self._head = None
+        self._tail = None
+        self._length = 0
         if nodes is not None:
+            self._length = len(nodes)
             node = Node(nodes.pop(0))
-            self.head = node
+            self._head = node
+            prev_node = node
             for elem in nodes:
                 node.next = Node(elem)
+                prev_node = node
                 node = node.next
+                node.prev = prev_node
+            self._tail = node
 
     def __str__(self):
         nodes = []
-        node = self.head
+        node = self._head
         while node is not None:
             nodes.append(node)
             node = node.next
@@ -33,14 +41,14 @@ class LinkedList:
 
     def __repr__(self):
         nodes = []
-        node = self.head
+        node = self._head
         while node is not None:
             nodes.append(node)
             node = node.next
         return 'LinkedList({})'.format(', '.join(map(str, nodes)))
 
     def __iter__(self):
-        node = self.head
+        node = self._head
         while node is not None:
             yield node
             node = node.next
@@ -48,7 +56,7 @@ class LinkedList:
     def __getitem__(self, index):
         if index < 0:
             raise IndexError("list index must be >= 0")
-        if not self.head:
+        if not self._head:
             raise Exception("list is empty")
         i = 0
         for current_node in self:
@@ -60,7 +68,7 @@ class LinkedList:
     def __setitem__(self, index, value):
         if index < 0:
             raise IndexError("list index must be >= 0")
-        if not self.head:
+        if not self._head:
             raise Exception("list is empty")
         i = 0
         for current_node in self:
@@ -73,78 +81,130 @@ class LinkedList:
     def __delitem__(self, index):
         if index < 0:
             raise IndexError("list index must be >= 0")
-        if not self.head:
+        if not self._head:
             raise Exception("list is empty")
         if index == 0:
-            self.head = self.head.next
+            self._head = self._head.next
+            self._head.prev = None
+            self._length -= 1
             return
-        i, previous_node = 0, self.head
+        if index == self._length - 1:
+            print(self._tail.prev, self._tail, self._tail.next)
+            self._tail = self._tail.prev
+            self._tail.next = None
+            self._length -= 1
+            return
+        i, previous_node = 0, self._head
         for current_node in self:
             if i == index:
                 previous_node.next = current_node.next
+                current_node.next.prev = previous_node
+                self._length -= 1
                 return
             previous_node = current_node
             i += 1
         raise IndexError("list index out of range")
 
     def appendleft(self, data):
-        node = Node(data)
-        node.next = self.head
-        self.head = node
+        new_node = Node(data)
+        if not self._head:
+            self._head = new_node
+            self._tail = new_node
+            self._length += 1
+            return
+        new_node.next = self._head
+        self._head.prev = new_node
+        self._head = new_node
+        self._length += 1
 
     def append(self, data):
         new_node = Node(data)
-        if not self.head:
-            self.head = new_node
+        if not self._head:
+            self._head = new_node
+            self._tail = new_node
+            self._length += 1
             return
-        for current_node in self:
-            pass
-        current_node.next = new_node
+        self._tail.next = new_node
+        new_node.prev = self._tail
+        self._tail = new_node
+        self._length += 1
 
     def insert(self, index, data):
-        if not self.head or index <= 0:
+        if not self._head or index <= 0:
             self.appendleft(data)
+            return
+        if index > self._length - 1:
+            self.append(data)
             return
         new_node, i = Node(data), 1
         for current_node in self:
             if i == index:
                 new_node.next = current_node.next
+                new_node.prev = current_node
+                current_node.next.prev = new_node
                 current_node.next = new_node
+                self._length += 1
                 return
             i += 1
-        current_node.next = new_node
 
     def remove(self, data):
-        if not self.head:
+        if not self._head:
             raise Exception("list is empty")
-        if self.head.data == data:
-            self.head = self.head.next
+        if self._head.data == data:
+            del self[0]
             return
-        previous_node = self.head
+        if self._tail.data == data:
+            del self[self._length - 1]
+            return
+        previous_node = self._head
         for current_node in self:
             if current_node.data == data:
                 previous_node.next = current_node.next
+                current_node.next.prev = previous_node
+                self._length -= 1
                 return
             previous_node = current_node
         raise Exception("node with data '%s' not found" % data)
 
-    def pop(self, index):
+    def pop(self, index=None):
+        if index is None or index == self._length - 1:
+            pop_item = self._tail.data
+            del self[self._length - 1]
+            return pop_item
         if index < 0:
             raise IndexError("list index must be >= 0")
-        if not self.head:
+        if not self._head:
             raise Exception("list is empty")
         if index == 0:
-            pop_item = self.head.data
-            self.head = self.head.next
+            pop_item = self._head.data
+            del self[0]
             return pop_item
-        i, previous_node = 0, self.head
+        i, previous_node = 0, self._head
         for current_node in self:
             if i == index:
                 previous_node.next = current_node.next
+                current_node.next.prev = previous_node
+                self._length -= 1
                 return current_node.data
             previous_node = current_node
             i += 1
         raise IndexError("list index out of range")
+
+    @property
+    def head(self):
+        if not self._head:
+            return None
+        return self._head.data
+
+    @property
+    def tail(self):
+        if not self._tail:
+            return None
+        return self._tail.data
+
+    @property
+    def length(self):
+        return self._length
 
 
 def main():
@@ -154,12 +214,16 @@ def main():
     llist.append(3)
     llist.insert(1, 2)
     print(repr(llist))
+    print(llist.head)
     llist = LinkedList([10, 2, 'car', -4.4, 0])
     print(llist)
+    print(llist.length)
     llist[4] = 1
     llist.remove(-4.4)
     del llist[0]
     print(' '.join(map(str, llist)))
+    print(llist.tail)
+    print(llist.length)
     return llist
 
 
--- Programming/Sem4/Lab4/linked_list/test_llist.py
+++ Programming/Sem4/Lab4/linked_list/test_llist.py
@@ -21,8 +21,9 @@ def test_llist_init_str_repr(llist):
     second_node = Node('a')
     assert str(llist) == 'None'
     assert repr(llist) == 'LinkedList()'
-    llist.head = first_node
+    llist._head = first_node
     first_node.next = second_node
+    llist._tail = second_node
     assert str(llist) == '1 -> a -> None'
     assert repr(llist) == 'LinkedList(1, a)'
     llist = LinkedList([2, 'b'])
@@ -154,7 +155,7 @@ def test_pop(llist):
     item = llist.pop(0)
     assert item == -10.1
     assert repr(llist) == 'LinkedList(2, car, 4.4, 0)'
-    item = llist.pop(3)
+    item = llist.pop()
     assert item == 0
     assert repr(llist) == 'LinkedList(2, car, 4.4)'
     item = llist.pop(1)
@@ -164,3 +165,71 @@ def test_pop(llist):
         llist.pop(100)
     with pytest.raises(IndexError):
         llist.pop(-1)
+
+
+def test_head(llist):
+    assert repr(llist) == 'LinkedList()'
+    assert llist.head is None
+    llist.append(1)
+    assert llist.head == 1
+    llist = LinkedList([-10.1, 2, 'car', 4.4, 0])
+    assert llist.head == -10.1
+    llist.pop(4)
+    del llist[0]
+    llist.pop(0)
+    assert repr(llist) == 'LinkedList(car, 4.4)'
+    assert llist.head == 'car'
+    llist.remove('car')
+    assert llist.head == 4.4
+    llist[0] = -7.8
+    assert llist.head == -7.8
+    llist.insert(0, 'check')
+    assert llist.head == 'check'
+    llist.appendleft(3.0)
+    assert llist.head == 3.0
+    with pytest.raises(AttributeError):
+        llist.head = None
+
+
+def test_tail(llist):
+    assert repr(llist) == 'LinkedList()'
+    assert llist.tail is None
+    llist.appendleft(1)
+    assert llist.tail == 1
+    llist = LinkedList([0, 4.4, 'car', 2, -10.1])
+    assert llist.tail == -10.1
+    llist.pop(0)
+    del llist[3]
+    llist.pop()
+    assert repr(llist) == 'LinkedList(4.4, car)'
+    assert llist.tail == 'car'
+    llist.remove('car')
+    assert llist.tail == 4.4
+    llist[0] = -7.8
+    assert llist.tail == -7.8
+    llist.insert(1, 'check')
+    assert llist.tail == 'check'
+    llist.append(3.0)
+    assert llist.tail == 3.0
+    with pytest.raises(AttributeError):
+        llist.tail = None
+
+
+def test_length(llist):
+    assert repr(llist) == 'LinkedList()'
+    assert llist.length == 0
+    llist.appendleft(1)
+    assert llist.length == 1
+    llist = LinkedList([4.4, 'car', 2, -10.1])
+    assert llist.length == 4
+    del llist[3]
+    llist.pop()
+    llist.remove('car')
+    assert repr(llist) == 'LinkedList(4.4)'
+    assert llist.length == 1
+    llist.insert(1, 'check')
+    assert llist.length == 2
+    llist.append(3.0)
+    assert llist.length == 3
+    with pytest.raises(AttributeError):
+        llist.length = 10
