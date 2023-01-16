10f704e7b92410edb6e96803237cd9f081d3472e
Nazar Ponochevnyi <nazar.ponochevnyi@gmail.com>
2023-01-16 09:43:05 -0500

    fix: lab7

--- Data_Intensive_Apps/Lab7/lab7.sql
+++ Data_Intensive_Apps/Lab7/lab7.sql
@@ -75,10 +75,11 @@ INSERT INTO keyspace3.items (id, name, price, manufacturer, category, properties
 INSERT INTO keyspace3.items (id, name, price, manufacturer, category, properties) VALUES (7,'Samsung Galaxy Watch', 350, 'Samsung', 'Smart Watch', {'display':'AMOLED','storage':'16GB','water_resistant':'Yes','heart_rate_monitor':'Yes'});
 INSERT INTO keyspace3.items (id, name, price, manufacturer, category, properties) VALUES (8,'Fitbit Versa 3', 300, 'Fitbit', 'Smart Watch', {'display':'LCD','storage':'4GB','water_resistant':'Yes'});
 APPLY BATCH;
+
 nodetool status;
 
 -- select all rows from the table 'items'
-SELECT * FROM keyspace1.items WHERE category='Phone' AND manufacturer = 'Apple';
+SELECT * FROM keyspace1.items WHERE category = 'Phone' AND manufacturer = 'Apple';
 
 -- display the nodes on which the data is stored
 nodetool getendpoints keyspace1 items "Phone";
@@ -89,19 +90,41 @@ nodetool getendpoints keyspace3 items "Phone";
 nodetool disablegossip;
 nodetool disablethrift;
 
--- select all rows from the table 'items' in keyspace 'mykeyspace' where the category is 'Phone' and the color property is 'Gray' with a consistency level of QUORUM
-SELECT * FROM mykeyspace.items WHERE category='Phone' AND properties['color'] = 'Gray' ALLOW FILTERING;
+ALTER KEYSPACE keyspace1 WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1} AND DURABLE_WRITES = true AND read_repair_chance = 1.0 AND read_consistency='QUORUM' AND write_consistency='TWO';
 
--- insert a new row into the table 'items' in keyspace 'mykeyspace' with a consistency level of TWO
-INSERT INTO mykeyspace.items (id, name, price, manufacturer, category, properties) VALUES (4, 'LG V60', 900, 'LG', 'Phone', {'screen_size':'6.8 inches','storage':'128GB','color':'White'}) USING CONSISTENCY TWO;
+ALTER KEYSPACE keyspace2 WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 2} AND DURABLE_WRITES = true AND read_repair_chance = 1.0 AND read_consistency='ALL' AND write_consistency='ALL';
 
--- alter the keyspace 'mykeyspace'  with a replication factor of 3 and sets durable writes, default time to live, gc grace seconds and read repair chance.
-ALTER KEYSPACE mykeyspace WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 3} AND DURABLE_WRITES = true AND default_time_to_live = 0 AND gc_grace_seconds = 864000 AND read_repair_chance = 1.0;
+/*
+sudo nano /etc/iptables/iptables.rules
+sudo iptables -F
+sudo iptables -A INPUT -i lo -j ACCEPT 
+sudo iptables -A OUTPUT -o lo -j ACCEPT
+sudo iptables -A INPUT -j DROP
+sudo iptables -A OUTPUT -j DROP
+sudo iptables-save > /etc/iptables/iptables.rules
+sudo service iptables restart
+sudo iptables -L
+*/
 
--- insert values into the table 'items' in keyspace 'mykeyspace' with different properties
-INSERT INTO mykeyspace.items (id, name, price, manufacturer, category, properties) VALUES (5, 'LG V60', 800, 'LG', 'Phone', {'screen_size':'6.8 inches','storage':'128GB','color':'White'});
-INSERT INTO mykeyspace.items (id, name, price, manufacturer, category, properties) VALUES (5, 'LG V60', 900, 'LG', 'Phone', {'screen_size':'6.8 inches','storage':'128GB','color':'Black'});
+-- alter the keyspace3 with a replication factor of 3
+ALTER KEYSPACE keyspace3 WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 3} AND read_consistency='ONE' AND write_consistency='ONE';
 
--- lightweight transaction that will insert a new row into the table 'items' in keyspace 'mykeyspace' only if it does not already exist
-INSERT INTO mykeyspace.items (id, name, price, manufacturer, category, properties) VALUES (6, 'LG V60', 900, 'LG', 'Phone', {'screen_size':'6.8 inches','storage':'128GB','color':'White'}) IF NOT EXISTS;
+INSERT INTO keyspace3.items (id, name, price, manufacturer, category, properties) VALUES (5, 'LG V60', 800, 'LG', 'Phone', {'screen_size':'6.8 inches','storage':'128GB','color':'White'});
+INSERT INTO keyspace3.items (id, name, price, manufacturer, category, properties) VALUES (5, 'LG V60', 900, 'LG', 'Phone', {'screen_size':'6.8 inches','storage':'128GB','color':'Black'});
 
+-- lightweight transaction 
+INSERT INTO keyspace3.items (id, name, price, manufacturer, category, properties) VALUES (6, 'LG V70', 950, 'LG', 'Phone', {'screen_size':'6.8 inches','storage':'128GB','color':'White'}) IF NOT EXISTS;
+INSERT INTO keyspace3.items (id, name, price, manufacturer, category, properties) VALUES (6, 'LG V70', 990, 'LG', 'Phone', {'screen_size':'6.8 inches','storage':'128GB','color':'Black'}) IF NOT EXISTS;
+
+/*
+sudo iptables -F
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -F
+sudo iptables-save > /etc/iptables/iptables.rules
+sudo service iptables restart
+sudo iptables -L
+*/
+
+-- check how cassandra resolved the conflict
+SELECT * FROM keyspace3.items WHERE category = 'Phone' AND manufacturer = 'LG' AND id >= 5 AND id <= 6;
